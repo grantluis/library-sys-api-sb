91b340973f9fe3f257c7768846330912d237f3a1
Grant Emerson Luis <grant.luis@argano.com>
2024-04-02 16:08:06 +0800

    Initial commit.

---
+++ src/main/java/com/argano/librarysysapisb/LibrarysysapisbApplication.java
@@ -0,0 +1,13 @@
+package com.argano.librarysysapisb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarysysapisbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarysysapisbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/argano/librarysysapisb/LibrarysysapisbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.argano.librarysysapisb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarysysapisbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
